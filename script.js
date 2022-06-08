87818f4a06087010e58e37b81bd7b4f5f5c33dff
Shivam Katare <katare27451@gmail.com>
2022-06-09 00:26:32 +0530

    Make code more systematic

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-// Bcakground
-
-
-
+    // Bcakground
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["FullStack Web-Developer", "Technical Content Writer","Open Source Contributer", "DevOps Enthusiast", "Learner"],
+        strings: ["FullStack Web-Developer", "Technical Content Writer", "Open Source Contributer", "DevOps Enthusiast", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["FullStack Web-Developer", "Technical Content Writer","Open Source Contributer", "DevOps Enthusiast", "Learner"],
+        strings: ["FullStack Web-Developer", "Technical Content Writer", "Open Source Contributer", "DevOps Enthusiast", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -60,15 +57,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }

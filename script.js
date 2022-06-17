ffbf4a0d424df85a2ea39bb6c773434c4fd9a5c7
Shivam Katare <katare27451@gmail.com>
2022-06-17 16:53:11 +0530

    Add more projects, and modify all files

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
         } else {
             $('.navbar').removeClass("sticky");
         }
-
         // scroll-up button show/hide script
         if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
@@ -14,25 +13,21 @@ $(document).ready(function () {
             $('.scroll-up-btn').removeClass("show");
         }
     });
-
     // slide-up script
     $('.scroll-up-btn').click(function () {
         $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
-
     $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
-
     // toggle menu/navbar script
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
     // Bcakground
     // typing text animation script
     var typed = new Typed(".typing", {
@@ -41,14 +36,12 @@ $(document).ready(function () {
         backSpeed: 60,
         loop: true
     });
-
     var typed = new Typed(".typing-2", {
         strings: ["FullStack Web-Developer", "Technical Content Writer", "Open Source Contributer", "DevOps Enthusiast", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
